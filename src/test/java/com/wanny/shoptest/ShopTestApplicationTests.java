de5c8f6d13c2bdd9fe4e6abce97655ded232533c
wanny <a0926113302@gmail.com>
2024-06-08 15:05:21 +0800

    初始化 Spring Boot Project

---
+++ src/test/java/com/wanny/shoptest/ShopTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanny.shoptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
